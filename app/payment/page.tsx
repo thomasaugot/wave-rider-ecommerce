c7e5830679b2fd5fc92be89ef706008ac4f40de5
THOMAS AUGOT <thomas.augot@hotmail.fr>
2024-07-03 14:00:25 +0200

    payment info page ok

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -107,172 +107,191 @@ const PaymentPage = () => {
       <div className="checkout-container">
         <h2 className="section-heading">Payment Information</h2>
         <form className="payment-form" onSubmit={handleCheckout}>
-          <div className="delivery-info">
-            <h3>Delivery Information</h3>
-            <p>
-              <strong>Name:</strong> {deliveryInfo.fullName}
-            </p>
-            <p>
-              <strong>Address:</strong> {deliveryInfo.addressLine1},{" "}
-              {deliveryInfo.addressLine2 && `${deliveryInfo.addressLine2}, `}
-              {deliveryInfo.city}, {deliveryInfo.state},{" "}
-              {deliveryInfo.postalCode}
-            </p>
-            <p>
-              <strong>Delivery Option:</strong>{" "}
-              {deliveryInfo.deliveryOption.charAt(0).toUpperCase() +
-                deliveryInfo.deliveryOption.slice(1)}
-            </p>
-          </div>
-          <div className="billing-info">
-            <h3>Billing Information</h3>
-            <label>
-              <input
-                type="checkbox"
-                checked={useSameAddress}
-                onChange={() => setUseSameAddress(!useSameAddress)}
-              />
-              Use same address for billing
-            </label>
-            {!useSameAddress && (
-              <div className="billing-form">
-                <div className="form-group">
-                  <label htmlFor="billing-fullName">Full Name</label>
-                  <input
-                    type="text"
-                    id="billing-fullName"
-                    name="fullName"
-                    placeholder="Full Name"
-                    value={billingInfo.fullName}
-                    onChange={handleBillingInfoChange}
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="billing-addressLine1">Address Line 1</label>
-                  <input
-                    type="text"
-                    id="billing-addressLine1"
-                    name="addressLine1"
-                    placeholder="Address Line 1"
-                    value={billingInfo.addressLine1}
-                    onChange={handleBillingInfoChange}
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="billing-addressLine2">Address Line 2</label>
-                  <input
-                    type="text"
-                    id="billing-addressLine2"
-                    name="addressLine2"
-                    placeholder="Address Line 2"
-                    value={billingInfo.addressLine2}
-                    onChange={handleBillingInfoChange}
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="billing-city">City</label>
-                  <input
-                    type="text"
-                    id="billing-city"
-                    name="city"
-                    placeholder="City"
-                    value={billingInfo.city}
-                    onChange={handleBillingInfoChange}
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="billing-state">State</label>
-                  <input
-                    type="text"
-                    id="billing-state"
-                    name="state"
-                    placeholder="State"
-                    value={billingInfo.state}
-                    onChange={handleBillingInfoChange}
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="billing-postalCode">Postal Code</label>
-                  <input
-                    type="text"
-                    id="billing-postalCode"
-                    name="postalCode"
-                    placeholder="Postal Code"
-                    value={billingInfo.postalCode}
-                    onChange={handleBillingInfoChange}
-                    required
-                  />
-                </div>
-              </div>
-            )}
-          </div>
-          <div className="order-summary">
-            <h3>Order Summary</h3>
-            <p>Items Total: € {totalAmount}</p>
-            <p>Delivery: € {deliveryCost.toFixed(2)}</p>
-            <p>Total: € {totalWithDelivery}</p>
-          </div>
-          <div className="payment-method">
-            <h3>Choose Payment Method</h3>
-            <div className="payment-method__logos">
-              <Image src={VisaLogo} alt="Visa" width={60} height={40} />
-              <Image
-                src={MasterCardLogo}
-                alt="MasterCard"
-                width={60}
-                height={40}
-              />
-              <Image
-                src={AmexLogo}
-                alt="American Express"
-                width={60}
-                height={40}
-              />
+          <div className="right-col">
+            <div className="delivery-info">
+              <h3>Delivery Information</h3>
+              <p>
+                <strong>Name:</strong> {deliveryInfo.fullName}
+              </p>
+              <p>
+                <strong>Address:</strong> {deliveryInfo.addressLine1},{" "}
+                {deliveryInfo.addressLine2 && `${deliveryInfo.addressLine2}, `}
+                {deliveryInfo.city}, {deliveryInfo.state},{" "}
+                {deliveryInfo.postalCode}
+              </p>
+              <p>
+                <strong>Delivery Option:</strong>{" "}
+                {deliveryInfo.deliveryOption.charAt(0).toUpperCase() +
+                  deliveryInfo.deliveryOption.slice(1)}
+              </p>
             </div>
-            <div className="credit-card-inputs">
-              <div className="form-group">
-                <label htmlFor="cardNumber">Card Number</label>
+            <div className="billing-info">
+              <h3>Billing Information</h3>
+              <label>
                 <input
-                  type="text"
-                  id="cardNumber"
-                  name="cardNumber"
-                  placeholder="Card Number"
-                  required
+                  type="checkbox"
+                  checked={useSameAddress}
+                  onChange={() => setUseSameAddress(!useSameAddress)}
                 />
-              </div>
-              <div className="form-group">
-                <label htmlFor="expiryDate">Expiry Date</label>
-                <input
-                  type="text"
-                  id="expiryDate"
-                  name="expiryDate"
-                  placeholder="MM/YY"
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="cvc">CVC</label>
-                <input
-                  type="text"
-                  id="cvc"
-                  name="cvc"
-                  placeholder="CVC"
-                  required
-                />
-              </div>
+                Use same address for billing
+              </label>
+              {!useSameAddress && (
+                <div className="billing-form">
+                  <div className="form-group">
+                    <label htmlFor="billing-fullName">Full Name</label>
+                    <input
+                      type="text"
+                      id="billing-fullName"
+                      name="fullName"
+                      placeholder="Full Name"
+                      value={billingInfo.fullName}
+                      onChange={handleBillingInfoChange}
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="billing-addressLine1">Address Line 1</label>
+                    <input
+                      type="text"
+                      id="billing-addressLine1"
+                      name="addressLine1"
+                      placeholder="Address Line 1"
+                      value={billingInfo.addressLine1}
+                      onChange={handleBillingInfoChange}
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="billing-addressLine2">Address Line 2</label>
+                    <input
+                      type="text"
+                      id="billing-addressLine2"
+                      name="addressLine2"
+                      placeholder="Address Line 2"
+                      value={billingInfo.addressLine2}
+                      onChange={handleBillingInfoChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="billing-city">City</label>
+                    <input
+                      type="text"
+                      id="billing-city"
+                      name="city"
+                      placeholder="City"
+                      value={billingInfo.city}
+                      onChange={handleBillingInfoChange}
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="billing-state">State</label>
+                    <input
+                      type="text"
+                      id="billing-state"
+                      name="state"
+                      placeholder="State"
+                      value={billingInfo.state}
+                      onChange={handleBillingInfoChange}
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="billing-postalCode">Postal Code</label>
+                    <input
+                      type="text"
+                      id="billing-postalCode"
+                      name="postalCode"
+                      placeholder="Postal Code"
+                      value={billingInfo.postalCode}
+                      onChange={handleBillingInfoChange}
+                      required
+                    />
+                  </div>
+                </div>
+              )}
             </div>
           </div>
+          <div className="left-col">
+            <div className="order-summary">
+              <h3>Order Summary</h3>
+              <p>
+                <strong>Items Total:</strong> € {totalAmount}
+              </p>
+              <p>
+                <strong>Delivery:</strong> € {deliveryCost.toFixed(2)}
+              </p>
+              <p>
+                <strong>Total:</strong> € {totalWithDelivery}
+              </p>
+            </div>
+            <div className="payment-method">
+              <h3>Choose Payment Method</h3>
+              <div className="payment-method__logos">
+                <Image src={VisaLogo} alt="Visa" width={60} height={40} />
+                <Image
+                  src={MasterCardLogo}
+                  alt="MasterCard"
+                  width={60}
+                  height={40}
+                />
+                <Image
+                  src={AmexLogo}
+                  alt="American Express"
+                  width={60}
+                  height={40}
+                />
+              </div>
+              <div className="credit-card-inputs">
+                <div className="form-group">
+                  <label htmlFor="cardNumber">Card Number</label>
+                  <input
+                    type="text"
+                    id="cardNumber"
+                    name="cardNumber"
+                    placeholder="Card Number"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="expiryDate">Expiry Date</label>
+                  <input
+                    type="text"
+                    id="expiryDate"
+                    name="expiryDate"
+                    placeholder="MM/YY"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="cvc">CVC</label>
+                  <input
+                    type="text"
+                    id="cvc"
+                    name="cvc"
+                    placeholder="CVC"
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+        <div className="actions-container">
           <CustomButton
             text={loading ? "Processing..." : "Pay Now"}
             type="submit"
             disabled={loading}
             onClick={undefined}
           />
-        </form>
+          <CustomButton
+            text={"Back"}
+            type="submit"
+            disabled={loading}
+            onClick={undefined}
+            secondary={true}
+          />
+        </div>
       </div>
     </div>
   );

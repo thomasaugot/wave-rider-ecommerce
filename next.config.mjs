4fe7b942a73c08ec3440a1f6a4d48fb0085f2236
Thomas Augot <thomas.augot@hotmail.fr>
2024-10-01 12:33:38 +0100

    other attempt

--- next.config.mjs
+++ next.config.mjs
@@ -9,64 +9,55 @@ const withPWA = withPWAInit({
   dest: "public",
   register: true,
   skipWaiting: true,
-  extendDefaultRuntimeCaching: true,
-  workboxOptions: {
-    runtimeCaching: [
-      {
-        urlPattern: /\/static\/js\/main\.chunk\.js/,
-        handler: "CacheFirst",
-      },
-      {
-        urlPattern: /\/index\.html/,
-        handler: "StaleWhileRevalidate",
-      },
-      {
-        urlPattern: /\/images\/icons\/.*/,
-        handler: "CacheFirst",
-      },
-      {
-        urlPattern: /\/products/,
-        handler: "StaleWhileRevalidate",
-      },
-      {
-        urlPattern: /\/products\/.*/,
-        handler: "StaleWhileRevalidate",
-      },
-      {
-        urlPattern: /\/categories/,
-        handler: "StaleWhileRevalidate",
-      },
-      {
-        urlPattern: /\/brands/,
-        handler: "StaleWhileRevalidate",
-      },
-      {
-        urlPattern: /\/authentication/,
-        handler: "StaleWhileRevalidate",
-      },
-      {
-        urlPattern: /\/shopping-cart/,
-        handler: "StaleWhileRevalidate",
-      },
-      {
-        urlPattern: new RegExp(process.env.NEXT_PUBLIC_SUPABASE_URL),
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "supabase-api-cache",
-          expiration: {
-            maxEntries: 50,
-            maxAgeSeconds: 60 * 60 * 24,
-          },
+  runtimeCaching: [
+    {
+      urlPattern: /\/static\/js\/main\.chunk\.js/,
+      handler: "CacheFirst",
+    },
+    {
+      urlPattern: /\/index\.html/,
+      handler: "StaleWhileRevalidate",
+    },
+    {
+      urlPattern: /\/images\/icons\/.*/,
+      handler: "CacheFirst",
+    },
+    {
+      urlPattern: /\/products/,
+      handler: "StaleWhileRevalidate",
+    },
+    {
+      urlPattern: /\/products\/.*/,
+      handler: "StaleWhileRevalidate",
+    },
+    {
+      urlPattern: /\/categories/,
+      handler: "StaleWhileRevalidate",
+    },
+    {
+      urlPattern: /\/brands/,
+      handler: "StaleWhileRevalidate",
+    },
+    {
+      urlPattern: /\/authentication/,
+      handler: "StaleWhileRevalidate",
+    },
+    {
+      urlPattern: /\/shopping-cart/,
+      handler: "StaleWhileRevalidate",
+    },
+    {
+      urlPattern: new RegExp(process.env.NEXT_PUBLIC_SUPABASE_URL),
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "supabase-api-cache",
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 60 * 60 * 24, // Cache for 1 day
         },
       },
-    ],
-    ignoreURLParametersMatching: [/__WB_REVISION__/],
-  },
+    },
+  ],
 });
 
-export default withPWA({
-  reactStrictMode: false,
-  images: {
-    domains: ["github.com", "raw.githubusercontent.com", "i.pravatar.cc"],
-  },
-});
+export default withPWA(nextConfig);

687425f77cd162b0aaa10faa4ee257976b0e1824
Thomas Augot <thomas.augot@hotmail.fr>
2024-10-01 12:34:51 +0100

    other attempts

--- next.config.mjs
+++ next.config.mjs
@@ -9,55 +9,69 @@ const withPWA = withPWAInit({
   dest: "public",
   register: true,
   skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /\/static\/js\/main\.chunk\.js/,
-      handler: "CacheFirst",
-    },
-    {
-      urlPattern: /\/index\.html/,
-      handler: "StaleWhileRevalidate",
-    },
-    {
-      urlPattern: /\/images\/icons\/.*/,
-      handler: "CacheFirst",
-    },
-    {
-      urlPattern: /\/products/,
-      handler: "StaleWhileRevalidate",
-    },
-    {
-      urlPattern: /\/products\/.*/,
-      handler: "StaleWhileRevalidate",
-    },
-    {
-      urlPattern: /\/categories/,
-      handler: "StaleWhileRevalidate",
-    },
-    {
-      urlPattern: /\/brands/,
-      handler: "StaleWhileRevalidate",
-    },
-    {
-      urlPattern: /\/authentication/,
-      handler: "StaleWhileRevalidate",
-    },
-    {
-      urlPattern: /\/shopping-cart/,
-      handler: "StaleWhileRevalidate",
-    },
-    {
-      urlPattern: new RegExp(process.env.NEXT_PUBLIC_SUPABASE_URL),
-      handler: "NetworkFirst",
-      options: {
-        cacheName: "supabase-api-cache",
-        expiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 60 * 60 * 24, // Cache for 1 day
+  extendDefaultRuntimeCaching: true,
+  workboxOptions: {
+    runtimeCaching: [
+      // Cache the main JS bundle
+      {
+        urlPattern: /\/static\/js\/main\.chunk\.js/,
+        handler: "CacheFirst",
+      },
+      // Cache the main HTML file
+      {
+        urlPattern: /\/index\.html/,
+        handler: "StaleWhileRevalidate",
+      },
+      // Cache critical icons
+      {
+        urlPattern: /\/images\/icons\/.*/,
+        handler: "CacheFirst",
+      },
+      // Cache the /products page
+      {
+        urlPattern: /\/products/,
+        handler: "StaleWhileRevalidate",
+      },
+      // Cache all the dynamic /products/[id] pages
+      {
+        urlPattern: /\/products\/.*/,
+        handler: "StaleWhileRevalidate",
+      },
+      // Cache the /categories page
+      {
+        urlPattern: /\/categories/,
+        handler: "StaleWhileRevalidate",
+      },
+      // Cache the /brands page
+      {
+        urlPattern: /\/brands/,
+        handler: "StaleWhileRevalidate",
+      },
+      // Cache the /authentication page
+      {
+        urlPattern: /\/authentication/,
+        handler: "StaleWhileRevalidate",
+      },
+      // Cache the /shopping-cart page
+      {
+        urlPattern: /\/shopping-cart/,
+        handler: "StaleWhileRevalidate",
+      },
+      // Cache the API requests from Supabase using the NEXT_PUBLIC_SUPABASE_URL env variable
+      {
+        urlPattern: new RegExp(process.env.NEXT_PUBLIC_SUPABASE_URL),
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "supabase-api-cache",
+          expiration: {
+            maxEntries: 50,
+            maxAgeSeconds: 60 * 60 * 24, // Cache for 1 day
+          },
         },
       },
-    },
-  ],
+    ],
+    ignoreURLParametersMatching: [/__WB_REVISION__/],
+  },
 });
 
 export default withPWA(nextConfig);
